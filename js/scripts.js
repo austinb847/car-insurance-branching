ecb2011715b9f4efeeb86ad759f5fb240d03b148
austinb847 <austinb847@icloud.com>
2020-03-26 09:58:24 -0700

    checkif user entered there age into the form. if not then send an alert

--- js/scripts.js
+++ js/scripts.js
@@ -3,14 +3,17 @@ $(document).ready(function() {
     var age = parseInt($("input#age").val());
     var gender = $("select#gender").val();
 
-    var quote = (100 - age) * 3;
-    if (gender === 'male' || age < 26) {
+    if (age) {
+      var quote = (100 - age) * 3;
+      
+      if (gender === 'male' || age < 26) {
       quote += 50;
+      }
+      $("#rate").text(quote);
+      $("#quote").show();
+    } else {
+      alert('please enter your age');
     }
-
-    $("#rate").text(quote);
-    $("#quote").show();
-
     event.preventDefault();
   });
 });
